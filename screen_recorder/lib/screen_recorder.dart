47e310896fc95b31c99ce61f66fb645e1cea8efd
fzyzcjy <ch271828n@outlook.com>
2023-06-21 13:38:22 +0800

    simple MyCanvas that delegates

--- screen_recorder/lib/screen_recorder.dart
+++ screen_recorder/lib/screen_recorder.dart
@@ -1,7 +1,8 @@
-library screen_recorder;
+import 'package:flutter/material.dart';
+import 'package:screen_recorder/my_canvas.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class ScreenRecorder {
+  ScreenRecorder() {
+    PaintingContext.createCanvas = (recorder) => MyCanvas(Canvas(recorder));
+  }
 }

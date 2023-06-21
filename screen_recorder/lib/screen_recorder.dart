b7e8edd76b7f029619930a4d66065ce106657b64
fzyzcjy <ch271828n@outlook.com>
2023-06-21 16:47:45 +0800

    SimpleCompressor

--- screen_recorder/lib/screen_recorder.dart
+++ screen_recorder/lib/screen_recorder.dart
@@ -1,34 +1,28 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:screen_recorder/data_per_frame.dart';
 import 'package:screen_recorder/my_canvas.dart';
 import 'package:screen_recorder/my_paragraph_builder.dart';
+import 'package:screen_recorder/simple_compressor.dart';
 
 class ScreenRecorder {
+  static const _kTag = 'ScreenRecorder';
+
   static setup() {
     PaintingContext.createCanvas = (recorder) => MyCanvas(Canvas(recorder));
     TextPainter.createParagraphBuilder = (style) => MyParagraphBuilder(style);
 
     var overallUncompressedBytesLen = 0;
-    var overallCompressedBytesLen = 0;
-
-    final overallUncompressedBytes = StreamController<List<int>>();
-    overallUncompressedBytes //
-        .stream
-        .transform(ZLibEncoder())
-        .listen((bytes) => overallCompressedBytesLen += bytes.length);
+    final compressor = SimpleCompressor();
 
     SchedulerBinding.instance.addPersistentFrameCallback((timeStamp) {
-      print('PersistentFrameCallback '
+      print('$_kTag PersistentFrameCallback '
           'overallUncompressedBytesLen=$overallUncompressedBytesLen '
-          'overallCompressedBytesLen=$overallCompressedBytesLen '
-          'DataPerFrame=${DataPerFrame.instance}');
+          'compressor=$compressor '
+          'dataPerFrame=${DataPerFrame.instance}');
 
       overallUncompressedBytesLen += DataPerFrame.instance.bytes.len;
-      overallUncompressedBytes.add(DataPerFrame.instance.bytes.dataView());
+      compressor.add(DataPerFrame.instance.bytes.dataView());
 
       DataPerFrame.instance = DataPerFrame();
     });
---
+++ screen_recorder/lib/simple_compressor.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+class SimpleCompressor {
+  final _pendingChunks = <List<int>>[];
+  var _compressedBytesLen = 0;
+
+  int get pendingChunksBytesLen => _pendingChunks.fold(0, (a, b) => a + b.length);
+
+  void add(List<int> chunk) {
+    _pendingChunks.add(chunk);
+
+    if (pendingChunksBytesLen >= 200000) {
+      // ignore: deprecated_export_use
+      final pendingData = BytesBuilder(copy: false);
+      for (final chunk in _pendingChunks) {
+        pendingData.add(chunk);
+      }
+      _pendingChunks.clear();
+
+      final compressedData = ZLibEncoder().convert(pendingData.takeBytes());
+      _compressedBytesLen += compressedData.length;
+    }
+  }
+
+  @override
+  String toString() => 'SimpleCompressor{'
+      'pendingChunksBytesLen: $pendingChunksBytesLen, '
+      'compressedBytesLen: $_compressedBytesLen'
+      '}';
+}

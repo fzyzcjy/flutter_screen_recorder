0125526495a5abaf3363a68ed4a7b47696be642f
fzyzcjy <ch271828n@outlook.com>
2023-06-21 18:43:09 +0800

    empty MySceneBuilder

---
+++ screen_recorder/lib/my_scene_builder.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class MySceneBuilder implements SceneBuilder {
+  final SceneBuilder builder;
+
+  MySceneBuilder(this.builder);
+}
--- screen_recorder/lib/screen_recorder.dart
+++ screen_recorder/lib/screen_recorder.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
+
+import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:screen_recorder/data_per_frame.dart';
 import 'package:screen_recorder/my_canvas.dart';
 import 'package:screen_recorder/my_paragraph_builder.dart';
+import 'package:screen_recorder/my_scene_builder.dart';
 import 'package:screen_recorder/simple_compressor.dart';
 
 class ScreenRecorder {
@@ -11,6 +14,7 @@ class ScreenRecorder {
   static setup() {
     PaintingContext.createCanvas = (recorder) => MyCanvas(Canvas(recorder));
     TextPainter.createParagraphBuilder = (style) => MyParagraphBuilder(style);
+    RenderView.createSceneBuilder = () => MySceneBuilder(SceneBuilder());
 
     var overallUncompressedBytesLen = 0;
     final compressor = SimpleCompressor();

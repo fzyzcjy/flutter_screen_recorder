ed66b10290b97570a4a95cb6bb1b5af72958e1f5
fzyzcjy <ch271828n@outlook.com>
2023-06-21 15:31:22 +0800

    MyParagraphBuilder add count map

--- screen_recorder/lib/my_paragraph_builder.dart
+++ screen_recorder/lib/my_paragraph_builder.dart
@@ -1,9 +1,13 @@
 import 'dart:ui';
 
+import 'package:screen_recorder/data_per_frame.dart';
+
 class MyParagraphBuilder implements ParagraphBuilder {
   final ParagraphBuilder builder;
 
-  MyParagraphBuilder(this.builder);
+  MyParagraphBuilder(this.builder) {
+    DataPerFrame.instance.incrCount('ParagraphBuilder.new');
+  }
 
   @override
   void addPlaceholder(
@@ -14,6 +18,8 @@ class MyParagraphBuilder implements ParagraphBuilder {
     double? baselineOffset,
     TextBaseline? baseline,
   }) {
+    DataPerFrame.instance.incrCount('addPlaceholder');
+
     builder.addPlaceholder(
       width,
       height,
@@ -26,27 +32,31 @@ class MyParagraphBuilder implements ParagraphBuilder {
 
   @override
   void addText(String text) {
+    DataPerFrame.instance.incrCount('addText');
+
     builder.addText(text);
   }
 
   @override
-  Paragraph build() {
-    return builder.build();
+  void pop() {
+    DataPerFrame.instance.incrCount('pop');
+
+    builder.pop();
   }
+
+  @override
+  void pushStyle(TextStyle style) {
+    DataPerFrame.instance.incrCount('pushStyle');
+
+    builder.pushStyle(style);
+  }
+
+  @override
+  Paragraph build() => builder.build();
 
   @override
   int get placeholderCount => builder.placeholderCount;
 
   @override
   List<double> get placeholderScales => builder.placeholderScales;
-
-  @override
-  void pop() {
-    builder.pop();
-  }
-
-  @override
-  void pushStyle(TextStyle style) {
-    builder.pushStyle(style);
-  }
 }

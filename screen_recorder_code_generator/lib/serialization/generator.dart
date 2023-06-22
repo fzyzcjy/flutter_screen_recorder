f0c91d798932dc144b5db09ad6c7b6629f4578f7
fzyzcjy <ch271828n@outlook.com>
2023-06-22 12:05:15 +0800

    extract generate_class #9598

--- screen_recorder_code_generator/lib/serialization/generator.dart
+++ screen_recorder_code_generator/lib/serialization/generator.dart
@@ -1,22 +1,11 @@
 import 'dart:io';
 
-import 'package:code_builder/code_builder.dart';
 import 'package:recase/recase.dart';
-import 'package:screen_recorder_code_generator/method_replayer/generator.dart' as method_replayer;
-import 'package:screen_recorder_code_generator/serialization/config.dart';
-import 'package:screen_recorder_code_generator/serialization/config_data/from_method_replayer.dart';
-import 'package:screen_recorder_code_generator/serialization/config_data/manual.dart';
+import 'package:screen_recorder_code_generator/serialization/generator/generate_class.dart';
 import 'package:screen_recorder_code_generator/utils.dart';
 
-final kConfigs = [
-  ...kManualConfigs,
-  ...createConfigsFromMethodReplayer(method_replayer.kConfigs),
-];
-
 void generateAllSerialization(String dirTarget) {
-  final lines = <String>[];
-
-  lines.add('''
+  final text = '''
 $kCommentHeader
 
 import 'dart:typed_data';
@@ -26,49 +15,11 @@ import 'package:screen_recorder/bytes_reader.dart';
 import 'package:screen_recorder/generated/record/canvas.dart';
 import 'package:screen_recorder/generated/record/paragraph_builder.dart';
 import 'package:screen_recorder/generated/record/scene_builder.dart';
-  ''');
 
-  for (final config in kConfigs) {
-    lines.addAll([
-      generateFromBytes(config),
-      generateToBytes(config),
-    ]);
-  }
+${generateAllClass()}
+''';
 
-  File('$dirTarget/serialization/serialization.dart').writeAsStringSync(dartfmt.format(lines.join('\n')));
-}
-
-String generateFromBytes(Config config) {
-  final constructorCall = refer(config.className)
-      .call(
-        config.nonNamedFields.map((e) => refer(e.name)),
-        Map.fromEntries(config.namedFields.map((e) => MapEntry(e.name, refer(e.name)))),
-      )
-      .statement
-      .dartCode;
-
-  return '''
-${config.className} fromBytes${getSerializationPartialName(config.className)}(BytesReader reader) {
-  ${config.fields.map((e) => _generateFromBytesField(config, e)).join('\n')}
-  return $constructorCall
-}
-  ''';
-}
-
-String _generateFromBytesField(Config config, ConfigField configField) {
-  return 'final ${configField.name} = fromBytes${getSerializationPartialName(configField.type)}(reader);';
-}
-
-String generateToBytes(Config config) {
-  return '''
-void toBytes${getSerializationPartialName(config.className)}(BytesBuilder writer, ${config.className} value) {
-  ${config.fields.map((e) => _generateToBytesField(config, e)).join('\n')}
-}
-  ''';
-}
-
-String _generateToBytesField(Config config, ConfigField configField) {
-  return 'toBytes${getSerializationPartialName(configField.type)}(writer, value.${configField.name});';
+  File('$dirTarget/serialization/serialization.dart').writeAsStringSync(dartfmt.format(text));
 }
 
 String getSerializationPartialName(String type) {
---
+++ screen_recorder_code_generator/lib/serialization/generator/generate_class.dart
@@ -0,0 +1,56 @@
+import 'package:code_builder/code_builder.dart';
+import 'package:screen_recorder_code_generator/method_replayer/generator.dart' as method_replayer;
+import 'package:screen_recorder_code_generator/serialization/config.dart';
+import 'package:screen_recorder_code_generator/serialization/config_data/from_method_replayer.dart';
+import 'package:screen_recorder_code_generator/serialization/config_data/manual.dart';
+import 'package:screen_recorder_code_generator/serialization/generator.dart';
+import 'package:screen_recorder_code_generator/utils.dart';
+
+final kConfigs = [
+  ...kManualConfigs,
+  ...createConfigsFromMethodReplayer(method_replayer.kConfigs),
+];
+
+String generateAllClass() {
+  final lines = <String>[];
+  for (final config in kConfigs) {
+    lines.addAll([
+      _generateFromBytes(config),
+      _generateToBytes(config),
+    ]);
+  }
+  return lines.join('\n');
+}
+
+String _generateFromBytes(Config config) {
+  final constructorCall = refer(config.className)
+      .call(
+        config.nonNamedFields.map((e) => refer(e.name)),
+        Map.fromEntries(config.namedFields.map((e) => MapEntry(e.name, refer(e.name)))),
+      )
+      .statement
+      .dartCode;
+
+  return '''
+${config.className} fromBytes${getSerializationPartialName(config.className)}(BytesReader reader) {
+  ${config.fields.map((e) => _generateFromBytesField(config, e)).join('\n')}
+  return $constructorCall
+}
+  ''';
+}
+
+String _generateFromBytesField(Config config, ConfigField configField) {
+  return 'final ${configField.name} = fromBytes${getSerializationPartialName(configField.type)}(reader);';
+}
+
+String _generateToBytes(Config config) {
+  return '''
+void toBytes${getSerializationPartialName(config.className)}(BytesBuilder writer, ${config.className} value) {
+  ${config.fields.map((e) => _generateToBytesField(config, e)).join('\n')}
+}
+  ''';
+}
+
+String _generateToBytesField(Config config, ConfigField configField) {
+  return 'toBytes${getSerializationPartialName(configField.type)}(writer, value.${configField.name});';
+}

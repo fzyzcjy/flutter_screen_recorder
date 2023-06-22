54708bfc39f47e04720730e85f1112bfeb829566
fzyzcjy <ch271828n@outlook.com>
2023-06-22 17:13:09 +0800

    simple test int #9604

---
+++ screen_recorder/test/serialization_test.dart
@@ -0,0 +1,35 @@
+import 'dart:typed_data';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:screen_recorder/bytes_reader.dart';
+import 'package:screen_recorder/serialization.dart';
+
+void main() {
+  group('toBytes and fromBytes', () {
+    group('int', () {
+      for (final value in [0, 42, -42, 9223372036854775807, -9223372036854775808]) {
+        test('value=$value', () {
+          _body(
+            toBytes: toBytesInt,
+            fromBytes: fromBytesInt,
+            value: 42,
+          );
+        });
+      }
+    });
+  });
+}
+
+void _body<T extends Object>({
+  required void Function(BytesBuilder writer, T value) toBytes,
+  required T Function(BytesReader reader) fromBytes,
+  required T value,
+}) {
+  final writer = BytesBuilder();
+  toBytes(writer, value);
+  final bytes = writer.takeBytes();
+
+  final reader = BytesReader(bytes);
+  final recoveredValue = fromBytes(reader);
+  expect(recoveredValue, value);
+}

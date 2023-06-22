3b8292b7bc04eeedd176107c279bd788bd6e8099
fzyzcjy <ch271828n@outlook.com>
2023-06-22 17:52:10 +0800

    chore

--- screen_recorder/lib/replayer/paragraph_builder.dart
+++ screen_recorder/lib/replayer/paragraph_builder.dart
@@ -4,7 +4,7 @@ import 'package:screen_recorder/record_list.dart';
 
 class ParagraphBuilderReplayer {
   static Paragraph replay(ParagraphBuilderRecordList recordList) {
-    final builder = ParagraphBuilder(recordList.constructorRecord!.style);
+    final builder = ParagraphBuilder(recordList.constructorRecord.style);
     for (final record in recordList.methodCallRecords) {
       record.execute(builder);
     }

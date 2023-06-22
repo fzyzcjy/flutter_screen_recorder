f2253eabc98bbfbb49b9477d3310328af1f84277
fzyzcjy <ch271828n@outlook.com>
2023-06-22 20:31:46 +0800

    add placeholder implementations #9606

--- screen_recorder/lib/serialization/unsupported_types.dart
+++ screen_recorder/lib/serialization/unsupported_types.dart
@@ -1,30 +1,33 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart' as material;
 import 'package:screen_recorder/bytes_reader_writer.dart';
 import 'package:screen_recorder/placeholder_data.dart';
-
-// ======================= types with throw error =========================
-
-// TODO implement these
-
-ColorFilter fromBytesColorFilter(BytesReader reader) => throw UnimplementedError();
-
-void toBytesColorFilter(BytesWriter writer, ColorFilter value) {}
-
-ImageFilter fromBytesImageFilter(BytesReader reader) => throw UnimplementedError();
-
-void toBytesImageFilter(BytesWriter writer, ImageFilter value) {}
-
-Shader fromBytesShader(BytesReader reader) => throw UnimplementedError();
-
-void toBytesShader(BytesWriter writer, Shader value) {}
-
-Vertices fromBytesVertices(BytesReader reader) => throw UnimplementedError();
-
-void toBytesVertices(BytesWriter writer, Vertices value) {}
-
-// ======================= types with placeholder/dummy data =========================
 
 Image fromBytesImage(BytesReader reader) => PlaceholderData.instance.placeholderImage;
 
 void toBytesImage(BytesWriter writer, Image value) {}
+
+Vertices fromBytesVertices(BytesReader reader) =>
+    // dummy (empty) vertices
+    Vertices(VertexMode.triangles, []);
+
+void toBytesVertices(BytesWriter writer, Vertices value) {}
+
+ColorFilter fromBytesColorFilter(BytesReader reader) =>
+    // identity ColorFilter
+    const ColorFilter.matrix([1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0]);
+
+void toBytesColorFilter(BytesWriter writer, ColorFilter value) {}
+
+ImageFilter fromBytesImageFilter(BytesReader reader) =>
+    // identity filter
+    ImageFilter.matrix(material.Matrix4.identity().storage);
+
+void toBytesImageFilter(BytesWriter writer, ImageFilter value) {}
+
+Shader fromBytesShader(BytesReader reader) =>
+    // dummy shader
+    Gradient.radial(Offset.zero, 50, [material.Colors.red, material.Colors.blue]);
+
+void toBytesShader(BytesWriter writer, Shader value) {}

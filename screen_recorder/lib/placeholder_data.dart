ae68b699e82348311eed2a959088ba7dd35a24eb
fzyzcjy <ch271828n@outlook.com>
2023-06-22 20:27:33 +0800

    impl _createPlaceholderImage #9606

--- screen_recorder/lib/placeholder_data.dart
+++ screen_recorder/lib/placeholder_data.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:typed_data';
 import 'dart:ui' as ui;
 
 class PlaceholderData {
@@ -8,6 +10,14 @@ class PlaceholderData {
   late final ui.Image placeholderImage;
 
   Future<void> setup() async {
-    placeholderImage = TODO;
+    placeholderImage = await _createPlaceholderImage(100, 100);
   }
+}
+
+// ref: Flutter :: createTestImage
+Future<ui.Image> _createPlaceholderImage(int width, int height) async {
+  final Completer<ui.Image> completer = Completer<ui.Image>();
+  ui.decodeImageFromPixels(Uint8List.fromList(List<int>.filled(width * height * 4, 100)), width, height,
+      ui.PixelFormat.rgba8888, completer.complete);
+  return completer.future;
 }

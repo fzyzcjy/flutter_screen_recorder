0053771077fbd8dfb24e0bf7af78666544ec832d
fzyzcjy <ch271828n@outlook.com>
2023-06-23 21:16:29 +0800

    manually compute _pendingChunksBytesLen since have seen fold() in profiling #9649

--- screen_recorder/lib/src/simple_compressor.dart
+++ screen_recorder/lib/src/simple_compressor.dart
@@ -2,20 +2,23 @@ import 'dart:io';
 
 class SimpleCompressor {
   final _pendingChunks = <List<int>>[];
-  var _compressedBytesLen = 0;
+  var _pendingChunksBytesLen = 0;
 
-  int get pendingChunksBytesLen => _pendingChunks.fold(0, (a, b) => a + b.length);
+  var _compressedBytesLen = 0;
 
   void add(List<int> chunk) {
     _pendingChunks.add(chunk);
+    _pendingChunksBytesLen += chunk.length;
 
-    if (pendingChunksBytesLen >= 200000) {
+    if (_pendingChunksBytesLen >= 200000) {
       // ignore: deprecated_export_use
       final pendingData = BytesBuilder(copy: false);
       for (final chunk in _pendingChunks) {
         pendingData.add(chunk);
       }
+
       _pendingChunks.clear();
+      _pendingChunksBytesLen = 0;
 
       final compressedData = ZLibEncoder().convert(pendingData.takeBytes());
       _compressedBytesLen += compressedData.length;
@@ -24,7 +27,7 @@ class SimpleCompressor {
 
   @override
   String toString() => 'SimpleCompressor{'
-      'pendingChunksBytesLen: $pendingChunksBytesLen, '
+      'pendingChunksBytesLen: $_pendingChunksBytesLen, '
       'compressedBytesLen: $_compressedBytesLen'
       '}';
 }

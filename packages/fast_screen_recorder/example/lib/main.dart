d84f7c48d8c66f720d5937f1698e34cf97072257
fzyzcjy <ch271828n@outlook.com>
2023-06-24 14:10:30 +0800

    change storage folder

--- packages/fast_screen_recorder/example/lib/main.dart
+++ packages/fast_screen_recorder/example/lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 
 import 'package:fast_screen_recorder/fast_screen_recorder.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() {
@@ -39,10 +37,10 @@ class _MyAppState extends State<MyApp> {
             ListTile(
               title: const Text('Start'),
               onTap: () async {
-                final dir = await getApplicationDocumentsDirectory();
+                final dir = '${(await getExternalStorageDirectory())!.path}/fast_screen_recorder_experiment';
+                await Directory(dir).create(recursive: true);
                 setState(() =>
-                path =
-                '${dir!.path}/${DateTime.now().toIso8601String().replaceAll(".", "").replaceAll(":", "")}.mp4');
+                    path = '$dir/${DateTime.now().toIso8601String().replaceAll(".", "").replaceAll(":", "")}.mp4');
 
                 await _recorder.start(
                   path: File(path!),
@@ -61,11 +59,10 @@ class _MyAppState extends State<MyApp> {
             ),
             Expanded(
               child: ListView.builder(
-                itemBuilder: (context, index) =>
-                    ListTile(
-                      title: Text('Item $index'),
-                      subtitle: Text('Random text ${Random().nextInt(1000000000)}'),
-                    ),
+                itemBuilder: (context, index) => ListTile(
+                  title: Text('Item $index'),
+                  subtitle: Text('Random text ${Random().nextInt(1000000000)}'),
+                ),
               ),
             ),
           ],

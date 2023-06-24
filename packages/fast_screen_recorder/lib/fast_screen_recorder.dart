c4abe6768fc625b769f07a756e00498b6e8adf36
fzyzcjy <ch271828n@outlook.com>
2023-06-24 10:16:40 +0800

    rm code

--- packages/fast_screen_recorder/lib/fast_screen_recorder.dart
+++
@@ -1,8 +0,0 @@
-
-import 'fast_screen_recorder_platform_interface.dart';
-
-class FastScreenRecorder {
-  Future<String?> getPlatformVersion() {
-    return FastScreenRecorderPlatform.instance.getPlatformVersion();
-  }
-}

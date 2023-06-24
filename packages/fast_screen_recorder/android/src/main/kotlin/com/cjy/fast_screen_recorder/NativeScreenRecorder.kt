712db317a56c2362944197026efd7e7824158d92
fzyzcjy <ch271828n@outlook.com>
2023-06-24 11:21:04 +0800

    basic capture #9654

--- packages/fast_screen_recorder/android/src/main/kotlin/com/cjy/fast_screen_recorder/NativeScreenRecorder.kt
+++ packages/fast_screen_recorder/android/src/main/kotlin/com/cjy/fast_screen_recorder/NativeScreenRecorder.kt
@@ -1,12 +1,37 @@
 package com.cjy.fast_screen_recorder
 
+import android.app.Activity
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.view.PixelCopy
+
 object NativeScreenRecorder {
+    private val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+
     fun start(path: String) {
         TODO()
     }
 
-    fun capture() {
-        TODO()
+    fun capture(activity: Activity) {
+        val window = activity.window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            PixelCopy.request(
+                window,
+                bitmap,
+                { copyResult ->
+                    if (copyResult == PixelCopy.SUCCESS) {
+                        TODO()
+                    } else {
+                        TODO()
+                    }
+                },
+                Handler(Looper.getMainLooper())
+            )
+        } else {
+            throw IllegalStateException("only support >= Android O currently")
+        }
     }
 
     fun stop() {

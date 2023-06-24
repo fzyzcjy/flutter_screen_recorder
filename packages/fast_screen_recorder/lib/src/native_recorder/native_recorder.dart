52d7ccf25fd78837840a92d5e4029bc94be29cab
fzyzcjy <ch271828n@outlook.com>
2023-06-24 10:33:44 +0800

    introduce pigeon #9658

--- packages/fast_screen_recorder/lib/src/native_recorder/native_recorder.dart
+++ packages/fast_screen_recorder/lib/src/native_recorder/native_recorder.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
 
+import 'package:fast_screen_recorder/src/messages.dart';
+
 class NativeRecorder {
   static final instance = NativeRecorder._();
+
+  final _hostApi = FastScreenRecorderHostApi();
 
   NativeRecorder._();
 
   Future<void> start({
     required File path,
   }) async =>
-      throw UnimplementedError();
+      await _hostApi.start(StartRequest(path: path.path));
 
-  Future<void> stop() async => throw UnimplementedError();
+  Future<void> stop() async => await _hostApi.stop();
 }
---
+++ packages/fast_screen_recorder/pigeons/messages.dart
@@ -0,0 +1,23 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: '../lib/src/messages.dart',
+  kotlinOut: '../android/src/main/kotlin/com/cjy/fast_screen_recorder/Messages.kt',
+  swiftOut: '../ios/Classes/messages.swift',
+  kotlinOptions: KotlinOptions(
+    package: 'com.cjy.fast_screen_recorder',
+  ),
+  dartOptions: DartOptions(copyrightHeader: [
+    'ignore_for_file: avoid-non-null-assertion, prefer_constructors_over_static_methods, unused_field, combinators_ordering'
+  ]),
+))
+class StartRequest {
+  late String path;
+}
+
+@HostApi()
+abstract class FastScreenRecorderHostApi {
+  void start(StartRequest request);
+
+  void stop();
+}

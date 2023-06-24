4047c065d198b68a1b8b2a300c5befd562fddc38
fzyzcjy <ch271828n@outlook.com>
2023-06-24 10:58:44 +0800

    add lock to FastScreenRecorder #9654

--- packages/fast_screen_recorder/lib/src/recorder/recorder.dart
+++ packages/fast_screen_recorder/lib/src/recorder/recorder.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:fast_screen_recorder/src/native_recorder/native_recorder.dart';
+import 'package:synchronized/synchronized.dart.dart';
 
 class FastScreenRecorder {
   static final instance = FastScreenRecorder._();
@@ -13,27 +14,30 @@ class FastScreenRecorder {
 
   Timer? _timer;
 
+  final _lock = Lock();
+
   Future<void> start({
     required File path,
     int fps = 2,
-  }) async {
-    if (_recording) throw ArgumentError('cannot start since already recording');
-    _recording = true;
+  }) async =>
+      await _lock.synchronized(() async {
+        if (_recording) throw ArgumentError('cannot start since already recording');
+        _recording = true;
 
-    await NativeRecorder.instance.start(path: path);
-    _timer = Timer.periodic(Duration(milliseconds: 1000 ~/ fps), _handlePeriodicCall);
-  }
+        await NativeRecorder.instance.start(path: path);
+        _timer = Timer.periodic(Duration(milliseconds: 1000 ~/ fps), _handlePeriodicCall);
+      });
 
-  Future<void> stop() async {
-    if (!_recording) throw ArgumentError('cannot start since already recording');
-    _recording = false;
+  Future<void> stop() async => await _lock.synchronized(() async {
+        if (!_recording) throw ArgumentError('cannot start since already recording');
+        _recording = false;
 
-    await NativeRecorder.instance.stop();
-    _timer?.cancel();
-    _timer = null;
-  }
+        await NativeRecorder.instance.stop();
+        _timer?.cancel();
+        _timer = null;
+      });
 
-  void _handlePeriodicCall(Timer _) {
-    NativeRecorder.instance.capture();
-  }
+  void _handlePeriodicCall(Timer _) async => await _lock.synchronized(() async {
+        await NativeRecorder.instance.capture();
+      });
 }

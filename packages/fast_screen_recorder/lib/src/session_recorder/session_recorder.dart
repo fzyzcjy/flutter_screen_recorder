8b92efa55319ce97389da5c9b50e83d4ed6c566c
fzyzcjy <ch271828n@outlook.com>
2023-06-25 20:04:43 +0800

    add lock and recordingdata #9688

--- packages/fast_screen_recorder/lib/src/session_recorder/session_recorder.dart
+++ packages/fast_screen_recorder/lib/src/session_recorder/session_recorder.dart
@@ -2,11 +2,17 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:fast_screen_recorder/src/recorder/recorder.dart';
+import 'package:synchronized/synchronized.dart';
 
 class SessionRecorder {
   static final instance = SessionRecorder._();
 
   SessionRecorder._();
+
+  bool get recording => _recordingData != null;
+  _RecordingData? _recordingData;
+
+  final _lock = Lock();
 
   final _recorder = FastScreenRecorder.instance;
 
@@ -16,28 +22,47 @@ class SessionRecorder {
     int fps = 2,
     int bitrate = 80 * 1000,
     int iFrameInterval = 10,
-  }) async {
-    if (!await directory.exists()) throw ArgumentError('Please ensure directory=$directory exists');
+  }) async =>
+      await _lock.synchronized(() async {
+        if (recording) throw ArgumentError('cannot start since already recording');
 
-    await _recorder.start(
-      pathVideo: pathVideo,
-      pathMetadata: pathMetadata,
-      outputSize: outputSize,
-      fps: fps,
-      bitrate: bitrate,
-      iFrameInterval: iFrameInterval,
-    );
-   
-    TODO_auto_stop_start;
-  }
+        if (!await directory.exists()) throw ArgumentError('Please ensure directory=$directory exists');
 
-  Future<void> stop() async {
-    await _recorder.stop();
-  }
+        await _recorder.start(
+          pathVideo: pathVideo,
+          pathMetadata: pathMetadata,
+          outputSize: outputSize,
+          fps: fps,
+          bitrate: bitrate,
+          iFrameInterval: iFrameInterval,
+        );
+
+        _recordingData = _RecordingData(
+          directory: directory,
+        );
+
+        TODO_auto_stop_start;
+      });
+
+  Future<void> stop() async => await _lock.synchronized(() async {
+        if (!recording) throw ArgumentError('cannot stop since already not recording');
+
+        _recordingData = null;
+
+        await _recorder.stop();
+      });
 
   Future<List<File>> getRecords({
     required DateTime startTime,
     required DateTime endTime,
   }) async =>
       throw UnimplementedError();
+}
+
+class _RecordingData {
+  final Directory directory;
+
+  _RecordingData({
+    required this.directory,
+  });
 }

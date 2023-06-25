c872705969a93ea91b5173dc5545ea656709c1b8
fzyzcjy <ch271828n@outlook.com>
2023-06-25 22:41:16 +0800

    tests for get() #9698

--- packages/fast_screen_recorder/test/time_named_directory_manager_test.dart
+++ packages/fast_screen_recorder/test/time_named_directory_manager_test.dart
@@ -11,15 +11,15 @@ void main() {
     );
   });
 
-  group('prune', () {
-    void _prepare() {
-      fs.file(manager.getPathForTime(DateTime(2000))).writeAsStringSync('a' * 1000);
-      fs.file(manager.getPathForTime(DateTime(2001))).writeAsStringSync('b' * 1000);
-      fs.file(manager.getPathForTime(DateTime(2002))).writeAsStringSync('c' * 1000);
-    }
+  void _createSeveralFiles() {
+    fs.file(manager.getPathForTime(DateTime(2000))).writeAsStringSync('a' * 1000);
+    fs.file(manager.getPathForTime(DateTime(2001))).writeAsStringSync('b' * 1000);
+    fs.file(manager.getPathForTime(DateTime(2002))).writeAsStringSync('c' * 1000);
+  }
 
+  group('prune', () {
     test('when maxKeepSize big enough, should prune nothing', () async {
-      _prepare();
+      _createSeveralFiles();
       await manager.prune(maxKeepSize: 3001);
       expect(
         fs.directory(directory).listSync(),
@@ -32,7 +32,7 @@ void main() {
     });
 
     test('when maxKeepSize small enough, should prune everything', () async {
-      _prepare();
+      _createSeveralFiles();
       await manager.prune(maxKeepSize: 999);
       expect(
         fs.directory(directory).listSync(),
@@ -41,7 +41,7 @@ void main() {
     });
 
     test('when maxKeepSize median, should prune oldest content', () async {
-      _prepare();
+      _createSeveralFiles();
       await manager.prune(maxKeepSize: 2001);
       expect(
         fs.directory(directory).listSync(),
@@ -55,8 +55,42 @@ void main() {
   });
 
   group('get', () {
-    test('simple', () {
-      TODO;
+    test('when no arg, should get all', () {
+      _createSeveralFiles();
+      expect(
+        manager.get(),
+        [
+          manager.getPathForTime(DateTime(2000)),
+          manager.getPathForTime(DateTime(2001)),
+          manager.getPathForTime(DateTime(2002)),
+        ],
+      );
+    });
+
+    test('when time at middle of a file, should include that file', () {
+      _createSeveralFiles();
+
+      expect(
+        manager.get(
+          startTime: DateTime(2000, 7, 1),
+          endTime: DateTime(2002, 7, 1),
+        ),
+        [
+          manager.getPathForTime(DateTime(2000)),
+          manager.getPathForTime(DateTime(2001)),
+          manager.getPathForTime(DateTime(2002)),
+        ],
+      );
+
+      expect(
+        manager.get(
+          startTime: DateTime(2001, 7, 1),
+          endTime: DateTime(2001, 7, 2),
+        ),
+        [
+          manager.getPathForTime(DateTime(2001)),
+        ],
+      );
     });
   });
 }

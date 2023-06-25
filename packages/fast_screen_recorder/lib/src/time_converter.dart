e3b96cf2ffce4e1c0488ab9e13c2ab724fb0a92f
fzyzcjy <ch271828n@outlook.com>
2023-06-25 10:36:14 +0800

    fix error #9677

--- packages/fast_screen_recorder/lib/src/time_converter.dart
+++ packages/fast_screen_recorder/lib/src/time_converter.dart
@@ -6,9 +6,10 @@ import 'package:fixnum/fixnum.dart';
 abstract class TimeConverter {
   static Duration videoToWallclockTime(Duration videoTime, proto.RecorderMetadataPack metadata) {
     final frameInfos = metadata.video.frameInfos;
-    final interestIndex = -1 +
-        frameInfos.lowerBoundBy<num>(proto.VideoFrameInfo(videoTimestampMicros: Int64(videoTime.inMicroseconds)),
-            (e) => e.videoTimestampMicros.toInt());
+    final interestIndex = (-1 +
+            frameInfos.lowerBoundBy<num>(proto.VideoFrameInfo(videoTimestampMicros: Int64(videoTime.inMicroseconds)),
+                (e) => e.videoTimestampMicros.toInt()))
+        .clamp(0, frameInfos.length - 1);
     final interestInfo = frameInfos[interestIndex];
 
     return interestInfo.wallclockTimestamp + (videoTime - interestInfo.videoTimestamp);

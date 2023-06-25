7ef23bc710b0479ee7e3183fc18acee6bda2e49a
fzyzcjy <ch271828n@outlook.com>
2023-06-25 09:29:22 +0800

    copy from vector-based impl #9675

--- packages/fast_screen_recorder/lib/src/interaction/interaction_player.dart
+++ packages/fast_screen_recorder/lib/src/interaction/interaction_player.dart
@@ -13,6 +13,36 @@ class InteractionPlayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TODO;
+    return CustomPaint(
+      painter: _InteractionPainter(
+        pack: pack,
+        timestamp: timestamp,
+      ),
+    );
   }
+}
+
+class _InteractionPainter extends CustomPainter {
+  final proto.InteractionPack pack;
+  final Duration timestamp;
+
+  _InteractionPainter({
+    required this.pack,
+    required this.timestamp,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final painter = Paint()
+      ..color = Colors.grey.withAlpha(100)
+      ..style = PaintingStyle.fill;
+
+    for (final position in framePacket.touch.positions) {
+      canvas.drawCircle(position, 20, painter);
+    }
+  }
+
+  // for simplicity, always shouldRepaint...
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }

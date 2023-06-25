b30f760f052e78da762988e4f0b56b03c4b09e27
fzyzcjy <ch271828n@outlook.com>
2023-06-25 21:11:08 +0800

    export

--- packages/fast_screen_recorder/lib/fast_screen_recorder.dart
+++ packages/fast_screen_recorder/lib/fast_screen_recorder.dart
@@ -1,3 +1,5 @@
 export 'package:fast_screen_recorder/src/demo_widget.dart';
+export 'package:fast_screen_recorder/src/fast_screen_player_widget.dart';
+export 'package:fast_screen_recorder/src/fast_screen_recorder_widget.dart';
 export 'package:fast_screen_recorder/src/recorder/recorder.dart';
 export 'package:fast_screen_recorder/src/session_recorder/session_recorder.dart';
